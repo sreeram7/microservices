90362a25084b24bbdae0f834b72cdbeb05f2e7cc
sreeram7 <mandasriram.7@gmail.com>
2022-08-26 17:43:55 +0800

    first commit

---
+++ PhotoappDiscoveryService/src/test/java/com/mandasriram/photoapp/discovery/PhotoappDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mandasriram.photoapp.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoappDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
