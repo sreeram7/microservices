24aaa714a569b1bc132da6282bb1288c460675da
sreeram7 <mandasriram.7@gmail.com>
2022-09-10 17:30:42 +0800

    "Commiting"

--- PhotoAppApiUsers/src/main/java/com/mandasriram/photoapp/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/mandasriram/photoapp/api/users/ui/controllers/UsersController.java
@@ -1,5 +1,7 @@
 package com.mandasriram.photoapp.api.users.ui.controllers;
 
+import javax.validation.Valid;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,14 +17,12 @@ public class UsersController {
 	@GetMapping("/status/check")
 	public String status()
 	{
-		
 		return "working";
 	}
 	
 	@PostMapping
-	public String createuser( @RequestBody	UserCreateRequestModel userdetails)
+	public String createuser(@Valid @RequestBody	UserCreateRequestModel userdetails)
 	{
-		
 		
 		return "post mapping method is called ";
 	}
